f886d2f2ee0a6487e27ab78a449ac2f50e0cf787
NguyenThanh1995 <thanhnguyennguyen1995@gmail.com>
2020-11-25 21:20:40 +0700

    .

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const fs = require("fs")
 
 express.uuse(cors())
 express.route("/Packages.json", (req, res) => {
-   res.send(fs.readFileSync("./Packages.json", "utf8"))
+   res.json(JSON.parse(fs.readFileSync("./Packages.json", "utf8")))
 })
 express.route("/tweaks.json/:package", (req, res) => {
    if (fs.existsSync(`./tweaks.json/${package}.json`)) {
-      res.send(fs.readFileSync(`./tweaks.json/${package}.json`))
+      res.json(JSON.parse(fs.readFileSync(`./tweaks.json/${package}.json`)))
    } else {
       res.send("Not Found")
    }
