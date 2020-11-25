b29df06cc256be1865e91efc4bcdde4452d6438f
NguyenThanh1995 <thanhnguyennguyen1995@gmail.com>
2020-11-25 09:35:17 +0700

    .

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ const modules = require("./modules"),
    path = require("path"),
    fsExtra = require("fs-extra")
 
-   !(async () => {
-      console.clear()
+async function readDir() {
+   let countTweaks = 0;
+   let debs = []
 
-      console.log(chalk.grey("Reading debians..."))
+   if (!fs.existsSync(__dirname + "/debs") || !fs.lstatSync(__dirname + "/debs").isDirectory()) {
+      throw new Error(`${__dirname}/debs is not folder.`)
+   } else {
 
-      const debs = {};
-      let countTweaks = 0;
-      !(await Promise.all(modules.readDebs("./debs")
+      debs = await Promise.all(modules.readDebs(__dirname + "/debs")
          .map(async item => {
             const info = await modules.dpkgf(item)
-            let newpath = `${path.dirname(item)}/${info.Package}_${info.Version}.deb`
+            let newpath = `${path.dirname(item)}/${info.Package}@${info.Version}.deb`
             if (path.basename(item, ".deb") != path.basename(newpath, ".deb")) {
                if (fs.existsSync(newpath) && fs.statSync(item).birthtimeMs > fs.statSync(newpath).birthtimeMs) {
                   fs.unlinkSync(item)
@@ -27,97 +28,113 @@ const modules = require("./modules"),
             }
             return info
          })
-      ))
-      .forEach(item => {
-         if (item.Package in debs) {
-            debs[item.Package].push(item)
-         } else {
-            debs[item.Package] = [item]
-            countTweaks++
-         }
-      })
+      )
 
-      console.log(chalk.yellow(`Readed all debians. ${countTweaks} tweaks`))
+      return {
+         debs,
+         countTweaks: debs.reduce((prev, current, index) => {
 
-
-      /*
-         @not build found and new packages
-      */
-
-      if (fs.existsSync("./tweaks.json") && fs.lstatSync("./tweaks.json").isDirectory()) {
-         /*if (fs.readdirSync("./tweaks.json").length) {
-            console.log(chalk.grey("Removing tweaks.json/*..."))
-            fsExtra.emptyDirSync("./tweaks.json")
-            console.log(chalk.yellow("Removed tweaks.json/*"))
-         }*/
-      } else {
-         fs.mkdirSync("./tweaks.json")
-      }
-
-
-      const allPackages = {}
-      const tweaks_json = []
-
-      for (const package in debs) {
-         debs[package] = debs[package].sort((a, b) => a.Version > b.Version)
-
-         allPackages[package] = {
-            Name: debs[package][0].Name,
-            Icon: debs[package][0].Icon,
-            MD5sum: debs[package][0].MD5sum,
-            Section: debs[package][0].Section,
-            Versions: debs[package].map(e => e.Version),
-            birthtimeMs: debs[package][0].birthtimeMs
-         }
-
-         let oldData = {}
-         if (fs.existsSync(`./tweaks.json/${package}.json`)) {
-            oldData = require(`./tweaks.json/${package}.json`)
-         }
-
-         tweaks_json.push(await new Promise(async (res, rej) => {
-            const Version = []
-            for await (const item of debs[package]) {
-               const changelog = {
-                  v: item.Version,
-                  supprt: oldData?.Version?.find(e => e.v == item.Version)?.supprt || await modules.input(`${package} (${chalk.cyan(debs[package][0].Name)}) v${item.Version} support for iOS? `)
-               }
-
-               for (const key in item) {
-                  if (item[key] != debs[package][0][key]) {
-                     changelog[key] = item[key]
-                  }
-               }
-
-               Version.push(changelog)
+            if (!debs.slice(index + 1).find(item => item.Package == current.Package)) {
+               prev++
             }
-            fs.writeFile(`./tweaks.json/${package}.json`, JSON.stringify({
-               ...debs[package][0],
-               Version
-            }), err => err ? rej() : res())
-         }))
 
+            return prev
+         }, 0)
+      }
+   }
+}
+
+function createFolder(name) {
+
+   if (fs.existsSync(`${__dirname}/${name}`)) {
+      if (!fs.lstatSync(`${__dirname}/${name}`).isDirectory()) {
+         throw new Error(`${__dirname}/${name} is a file. Please remove this.`)
+      }
+   } else {
+      fs.mkdirSync(`${__dirname}/${name}`)
+   }
+
+}
+
+function findLastVersion(debs, identifier, index) {
+   const packages = []
+
+   for (const length = debs.length; index < length; index++) {
+      if (item.Package === identifier) {
+         packages.push({
+            Name: debs[index].Name,
+            Icon: debs[index].Icon,
+            MD5sum: debs[index].MD5sum,
+            Section: debs[index].Section,
+            Version: debs[index],
+            birthtimeMs: debs[index].birthtimeMs
+         })
+      }
+   }
+
+   return packages.sort((a, b) => a.Version > b.Version)
+}
+
+!(async () => {
+   console.clear()
+
+   console.log(chalk.grey("Reading debians..."))
+
+   const { debs, countTweaks } = await readDir()
+
+   console.log(chalk.yellow(`Readed all debians. ${countTweaks} tweaks`))
+
+
+   /*
+      @not build found and new packages
+   */
+
+   createFolder("tweaks.json")
+
+   const allPackages = {}
+   const tweaks_json = []
+
+   debs.forEach((package, index) => {
+      if (!(package.Package in allPackages)) {
+         allPackages[package.Package] = findLastVersion(debs, package.Package, index)
+      }
+
+      let oldData = {},
+         newlink
+      if (fs.existsSync(newlink = `${__dirname}/tweaks.json/${package.Package}@${package.Version}.json`)) {
+         oldData = require(newlink)
       }
 
 
-      // save all json
-      console.log(chalk.grey(`Writing package.json...`))
+      tweaks_json.push(await new Promise(async (res, rej) => {
+         const newPackage = { ...package }
+         newPackage.Support = oldData?.Version || await modules.input(`${package.Package}@${package.Version} (${chalk.cyan(debs[package][0].Name)})? `)
 
-      fs.writeFileSync("Packages.json", JSON.stringify(allPackages))
-      fs.writeFileSync("Packages.length", countTweaks + "")
 
-      console.log(chalk.yellow(`Writed done Packages.json!`))
-      console.log(chalk.grey(`Writing files json to tweaks.json...`))
+         fs.writeFile(`${__dirname}/tweaks.json/${package.Package}@${package.Version}.json`, JSON.stringify(newPackage), err => err ? rej() : res())
+      }))
 
-      console.log(chalk.green(`Writed tweaks.json (${(await Promise.all(tweaks_json)).length} tweaks)`))
+   })
 
-      console.log(chalk.grey("Running postman.sh..."))
-      await modules.exec("sh postman.sh")
 
-      console.log(chalk.green("Writed Packages and Packages.bz2"))
-      console.log(chalk.grey("Writing Release..."))
+   // save all json
+   console.log(chalk.grey(`Writing package.json...`))
 
-      fs.writeFileSync("Release", `Origin: ${env.Origin}
+   fs.writeFileSync("Packages.json", JSON.stringify(allPackages))
+   fs.writeFileSync("Packages.length", countTweaks + "")
+
+   console.log(chalk.yellow(`Writed done Packages.json!`))
+   console.log(chalk.grey(`Writing files json to tweaks.json...`))
+
+   console.log(chalk.green(`Writed tweaks.json (${(await Promise.all(tweaks_json)).length} tweaks)`))
+
+   console.log(chalk.grey("Running postman.sh..."))
+   await modules.exec("sh postman.sh")
+
+   console.log(chalk.green("Writed Packages and Packages.bz2"))
+   console.log(chalk.grey("Writing Release..."))
+
+   fs.writeFileSync("Release", `Origin: ${env.Origin}
 Label: ${env.Label}
 Suite: ${env.Suite}
 Version: ${env.Version}
@@ -129,8 +146,8 @@ MD5Sum:
  ${md5file.sync("./Packages")} ${fs.statSync("./Packages").size} Packages
  ${md5file.sync("./Packages.bz2")} ${fs.statSync("./Packages.bz2").size} Packages.bz2`)
 
-      console.log(chalk.green("Writed file Release"))
-   })()
+   console.log(chalk.green("Writed file Release"))
+})()
 
 
 // ok man
