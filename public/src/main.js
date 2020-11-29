e517f1bfa4ba78f9648f304ca73f2609d684e870
Nguyen Thanh <thanhnguyennguyeb1995@gmail.com>
2020-11-29 13:02:13 +0700

    .

--- public/src/main.js
+++ public/src/main.js
@@ -1,3 +1,5 @@
+import eruda from "eruda"
+eruda.init()
 import Vue from "vue"
 import App from "./App"
 import router from "./router"
@@ -9,7 +11,6 @@ Vue.prototype.$config = config
 
 Object.defineProperty(Vue.prototype, "$iOSVersion", {
    get() {
-      return "10.3.3"
       const match = (navigator.appVersion).split("OS ")
       return match && match.length > 1 ? match[1].split(" ")[0].replace("_", ".") : "unknown"
    }
